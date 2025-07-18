8f7da2f80005aaf3ee9e04825951f40cec641814
AzalinaF <faiazalina.2019@gmail.com>
2025-07-18 15:04:34 +0300

    init project

---
+++ AuthorizeRepo/AuthorizeRepo/AuthorizeRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthorizeRepoApp.swift
+//  AuthorizeRepo
+//
+//  Created by Азалина Файзуллина on 18.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct AuthorizeRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
